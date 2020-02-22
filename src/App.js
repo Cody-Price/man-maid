abbffccc32c5e661ebb9277f26f6222e2f7d6b14
Cody Price 2 <codyprice2@Codys-MacBook-Pro.local>
2020-02-22 10:03:59 -0700

    Add header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import './App.css';
+import HeaderComponent from './HeaderComponent/HeaderComponent';
+import MainComponent from './MainComponent/MainComponent';
 
 function App() {
   return (
     <div className="App">
-      <h1><span class="logo">Man Maid</span></h1>
-      <h4>(Under Construction)</h4>
-      <h3><span class="logo">Man Maid</span> will be a handyman message board where you will be able to
-        post, search, and get in contact with various handymen in your local area!</h3>
-      <h3>If you would like more information or a <span class="logo">Man Maid</span> right away please contact:</h3>
-      <div class="owner-contact">
-        <h2>Jim Spaulding</h2>
-        <a href="mailto:themanmaidofdenver@gmail.com">themanmaidofdenver@gmail.com</a>
-        <a href="tel:573-747-6270">573-747-6270</a>
-      </div>
+      <HeaderComponent></HeaderComponent>
+      <MainComponent></MainComponent>
     </div>
   );
 }
